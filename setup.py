3edadcf3d8ae7151cf30c8f494490e682ca33ba3
yafes <yafes7@yahoo.de>
2019-01-27 16:07:50 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'SyncsketchGUI',
+    version = '1.0.0',
+    url = 'https://github.com/Schizo/ssmaya.git',
+    author = 'Syncsketch',
+    author_email = "dev@syncsketch.com",
+    description = "Syncsketch GUI for Autodesk Maya"
+    packages = find_packages(),
+    install_requires = [],
+)
